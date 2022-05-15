3386c0cf8c89832b1a02c7a7ee432803bfa23b34
unknown <andreea.fertu1004@gmail.com>
2022-05-15 08:46:50 +0300

    first commit

---
+++ src/Data/Categories.js
@@ -0,0 +1,13 @@
+const Categories = [
+  { category: "Books", value: 10 },
+  { category: "Science and Nature", value: 17 },
+  { category: "Computer", value: 18 },
+  { category: "Mathematics", value: 19 },
+  { category: "Sports", value: 21 },
+  { category: "Geography", value: 22 },
+  { category: "History", value: 23 },
+  { category: "Politics", value: 24 },
+  
+];
+
+export default Categories;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import "./Header.css";
+// import female from "../../../public/female.png"
+// import male from '../../../public/male.png'
+
+const Header = ({avatar}) => {
+  console.log(avatar)
+  return (
+    <div className="header">
+      {avatar === 'f' ? <img src="/female.png" /> : <img src="/man.png" />}
+      <Link to="/" className="title_header">
+        Trivia Game
+      </Link>
+      <hr className="divider" />
+    </div>
+  );
+};
+
+export default Header;
